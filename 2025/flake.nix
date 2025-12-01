0281ebeba95ce42a3b17f57d15f9986e7ea4111c
landaudiogo <diogo.hewitt.landau@hotmail.com>
2025-12-01 11:48:34 +0100

    Setup 2025 environment

---
+++ 2025/flake.nix
@@ -0,0 +1,21 @@
+{
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/25.05";
+    };
+    outputs = { nixpkgs, ... }@inputs:
+        let
+            system = "x86_64-linux";
+            pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+            devShells.${system} = {
+                default = pkgs.mkShell {
+                    packages = with pkgs; [ 
+                        cargo 
+                        rustc 
+                        rust-analyzer 
+                    ];
+                };
+            };
+        };
+}
