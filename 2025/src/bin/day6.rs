78b872b68786aa4724ae249aec3e72fd3aa1a9d7
landaudiogo <diogo.hewitt.landau@hotmail.com>
2025-12-06 14:04:32 +0100

    2025: day6p2

--- 2025/src/bin/day6.rs
+++ 2025/src/bin/day6.rs
@@ -1,6 +1,6 @@
 use std::io::{self, Read};
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 enum Op {
     Add,
     Mul,
@@ -19,7 +19,11 @@ impl From<&str> for Op {
 fn main() {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input);
+    p1(&input);
+    p2(&input);
+}
 
+fn p1(input: &str) {
     let mut operands: Vec<Vec<i64>> = Vec::new();
     let mut operators: Vec<Op> = Vec::new();
 
@@ -52,7 +56,51 @@ fn main() {
         total += tmp;
     }
 
-    println!("{total}");
-    // println!("{:?}", operands[0]);
-    // println!("{:?}", operators);
+    println!("p1: {total}");
+}
+
+fn p2(input: &str) {
+    let mut grid: Vec<Vec<char>> = Vec::new();
+    for line in input.lines() {
+        grid.push(line.chars().collect());
+    }
+
+    let operators: String = grid.pop().unwrap().iter().collect();
+    let operators: Vec<Op> = operators
+        .split_whitespace()
+        .map(|c_op| Op::from(c_op))
+        .collect();
+
+    let mut op_idx = 0;
+    let mut op = operators[op_idx];
+    let mut tmp = if let Op::Add = op { 0 } else { 1 };
+    let mut total = 0;
+
+    for c in 0..grid[0].len() {
+        let mut col = String::new();
+        for r in 0..(grid.len()) {
+            if grid[r][c] == ' ' {
+                continue;
+            }
+            col.push(grid[r][c]);
+        }
+
+        if col.len() == 0 {
+            op_idx += 1;
+            op = operators[op_idx];
+            total += tmp;
+            tmp = if let Op::Add = op { 0 } else { 1 };
+            continue;
+        }
+
+        let val: i64 = col.parse().unwrap();
+        tmp = if let Op::Add = op {
+            tmp + val
+        } else {
+            tmp * val
+        };
+    }
+    total += tmp;
+
+    println!("p2: {total}");
 }
